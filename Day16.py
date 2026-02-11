d29d0e9359760335d012cb3d853b9a65831064b4
Joseph McCole <joemccole12@gmail.com>
2026-02-11 20:08:17 +0000

    Add Day16.py: simple Turtle Graphics setup
    Add a new Day16.py that creates a turtle Screen, prints the canvas height, sets a black background, window title and size, and calls exitonclick. The file also imports PrettyTable and instantiates a PrettyTable object (currently unused). Note: code after exitonclick will not run and the file lacks a trailing newline.

---
+++ Day16.py
@@ -0,0 +1,9 @@
+import PrettyTable
+from turtle import Screen
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.bgcolor("black")
+my_screen.title("Turtle Graphics")
+my_screen.setup(width=600, height=600)
+my_screen.exitonclick()
+x = PrettyTable.PrettyTable()
